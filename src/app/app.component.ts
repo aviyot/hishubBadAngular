0ba81f1e942a80d031547124d4254bbf443b1552
aviyot <aviyot@gmail.com>
2019-04-25 17:30:18 +0300

    start work layot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'hishub-bad';
+export class AppComponent implements OnInit {
+  title = "hishub-bad";
+  inputData;
+
+ngOnInit() {
+
+ this.inputData = {
+    client: { label: "Client", placeholder: "Enter Client", type: "text", inputClass:"input" },
+    date  : { label: "Date", placeholder: "Enter Data", type: "date" , inputClass:"input"},
+
+  };
+}
 }
---
+++ src/app/components/input/input.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, Input } from "@angular/core";
+
+@Component({
+  selector: "app-input",
+  templateUrl: "./input.component.html",
+  styleUrls: ["./input.component.css"]
+})
+export class InputComponent implements OnInit {
+  @Input() inputData;
+  constructor() {}
+
+  ngOnInit() {}
+}
