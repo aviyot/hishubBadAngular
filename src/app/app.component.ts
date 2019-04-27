1be2505a7008033a653b0e822af3ebb21daea27d
aviyot <aviyot@gmail.com>
2019-04-27 16:40:55 +0300

    working in main layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,21 +13,23 @@ export class AppComponent implements OnInit {
 ngOnInit() {
 
  this.inputData = {
-    client: { label: "Client", placeholder: "Enter Client", type: "text", inputClass:"input" },
-    date: { label: "Date", placeholder: "Enter Client", type: "date", inputClass:"input" },
-    orderNum: { label: "OrderNum", placeholder: "Enter Client", type: "text", inputClass:"input" },
-    kant  : { label: "Kant", placeholder: "Enter Data", type: "number" , inputClass:"input"},
-    hibur  : { label: "Hibur", placeholder: "Enter Hibur", type: "number" , inputClass:"input"},
-    hebel  : { label: "Hebel", placeholder: "Enter Heble", type: "number" , inputClass:"input"},
-    tfiraType  : { label: "Type Tfrira", placeholder: "Choice Tfira", type: "select" , inputClass:"input"},
-    badName : { label: "Name", placeholder: "Choice Bad", type: "text" , inputClass:"input"},
-    badWidth  : { label: "Width Galil", placeholder: "", type: "number" , inputClass:"input"},
-    meterRoll  : { label: "Meter In Galil", placeholder: "", type: "number" , inputClass:"input"},
-    midaWidth  : { label: "Width Mida", placeholder: "Width", type: "number" , inputClass:"input"},
-    midaLength : { label: "Length Mida", placeholder: "Length", type: "number" , inputClass:"input"},
-    volanLength  : { label: "Length Volan", placeholder: "Length", type: "number" , inputClass:"input"},
-    volanType  : { label: "Type Volan", placeholder: "Type", type: "text" , inputClass:"input"},
-    sharvulLength : { label: "Length Sharvul", placeholder: "Length", type: "number" , inputClass:"input"},
+    client: { label: "Client", placeholder: "Enter Client", type: "text", inputClass:"input"  ,value:"" },
+    date: { label: "Date", placeholder: "Enter Client", type: "date", inputClass:"input"  ,value:""},
+    orderNum: { label: "OrderNum", placeholder: "Enter Client", type: "text", inputClass:"input"  ,value:""},
+    kant  : { label: "Kant", placeholder: "Enter Data", type: "number" , inputClass:"input"  ,value:""},
+    hibur  : { label: "Hibur", placeholder: "Enter Hibur", type: "number" , inputClass:"input"  ,value:""},
+    hebel  : { label: "Hebel", placeholder: "Enter Heble", type: "number" , inputClass:"input"  ,value:""},
+    tfiraType  : { label: "Type Tfrira", placeholder: "Choice Tfira", type: "select" , inputClass:"input"  ,value:""},
+    badName : { label: "Name Bad", placeholder: "Choice Bad", type: "text" , inputClass:"input"  ,value:""},
+    badWidth  : { label: "Width Galil", placeholder: "", type: "number" , inputClass:"input"  ,value:""},
+    meterRoll  : { label: "Meter In Galil", placeholder: "", type: "number" , inputClass:"input"  ,value:""},
+    midaWidth  : { label: "Width Mida", placeholder: "Width", type: "number" , inputClass:"input"  ,value:""},
+    midaLength : { label: "Length Mida", placeholder: "Length", type: "number" , inputClass:"input"  ,value:""},
+    volanLength  : { label: "Length Volan", placeholder: "Length", type: "number" , inputClass:"input"  ,value:""},
+    volanType  : { label: "Type Volan", placeholder: "Type", type: "text" , inputClass:"input"  ,value:""},
+    sharvulLength : { label: "Length Sharvul", placeholder: "Length", type: "number" , inputClass:"input" ,value:""},
+    sheritBtn: { label: "", placeholder: "Length", type: "button" , inputClass:"button",value :"Sherit"},
+    lengthBtn: { label: "", placeholder: "Length", type: "button" , inputClass:"button",value:"Length"}
   };
 }
 }
