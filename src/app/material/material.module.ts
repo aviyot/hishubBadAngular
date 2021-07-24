4b1ce87802acb27fa5457b563156eb67bd812609
aviyot <aviyot@gmail.com>
2021-07-24 13:03:44 +0300

    run ng update @angular/material@9 ng add @angular/localize

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from "@angular/core";
 import { MatCardModule } from "@angular/material/card";
 import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatInputModule, MatGridListModule } from "@angular/material";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatInputModule } from "@angular/material/input";
 import { MatButtonModule } from "@angular/material/button";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatSelectModule } from "@angular/material/select";
